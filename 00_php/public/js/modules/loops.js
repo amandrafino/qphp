a455e89720a7dc0ac6b6c5567282b42b84be5a7e
amandrafino <amandrafino@gmail.com>
2025-05-06 17:16:01 -0400

    help

--- 00_php/public/js/modules/loops.js
+++
@@ -1,7 +0,0 @@
-console.log('\n=== LOOP PRACTICE ===\n');
-
-console.log("This is loops.js");
-for (let i = 1; i <= 5; i++) {
-  console.log(`Loop iteration: ${i}`);
-}
-
--- 00_php/public/js/script.js
+++
@@ -1,6 +0,0 @@
-// Just change the import path based on what you're practicing
-import './modules/loops.js';
-import './modules/conditionals.js'
-
-// or: import './modules/conditionals.js';
-
