c75b25cb5160e89401dbe9fad267ceadee7d28fe
amandrafino <amandrafino@gmail.com>
2025-05-06 16:13:17 -0400

    first commit

---
+++ 00_php/public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Hello Page</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="centered-content">
+    <h1>Welcome to Project QPHP - 00_PHP</h1>
+    <h3>Project: JavaScript</h3>
+  </div>
+</body>
+
+  <script type="module" src="js/script.js"></script>
+</body>
+</html>
+
